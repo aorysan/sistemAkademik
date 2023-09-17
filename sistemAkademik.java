4a8407a4475cde4c1b559950b8cbe29da3ae9d70
VozSoldat <muhamaderril7@gmail.com>
2023-09-17 19:00:48 +0700

    Mengganti int ke double.

--- sistemAkademik.java
+++ sistemAkademik.java
@@ -7,9 +7,9 @@ public class sistemAkademik {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            int nMat, nIpa, nBing, nBin, nUHMat, nUHIpa, nUHBing, nUHBin, nUMat, nUIpa, nUBing, nUBin, bMat, bIpa, bBing, bBin;
-            int jmlPenilaian = 3;
-            double rataRata, indeksPrestasi, nAMat, nAIpa, nABing, nABin;
+            double nMat, nIpa, nBing, nBin, nUHMat, nUHIpa, nUHBing, nUHBin, nUMat, nUIpa, nUBing, nUBin, bMat, bIpa, bBing, bBin;
+            double jmlPenilaian = 3;
+            double rataRata, indeksPrestasi, nAMat, nAIpa, nABing, nABin; //int ganti ke double karena input nilai bisa berupa pecahan
             char nilaiHuruf;
                 bMat  = 3;
                 bIpa = 3;
@@ -18,29 +18,29 @@ public class sistemAkademik {
                 
                 
         System.out.println("Nilai Tugas Matematika : ");
-            nMat = sc.nextInt();
+            nMat = sc.nextDouble();
         System.out.println("Nilai Tugas IPA : ");
-            nIpa = sc.nextInt();
+            nIpa = sc.nextDouble();
         System.out.println("Nilai Tugas Bahasa Inggris : ");
-            nBing = sc.nextInt();
+            nBing = sc.nextDouble();
         System.out.println("Nilai Tugas Bahasa Indonesia : ");
-            nBin = sc.nextInt();
+            nBin = sc.nextDouble();
         System.out.println("Nilai Ulangan Harian Matematika : ");
-            nUHMat = sc.nextInt();
+            nUHMat = sc.nextDouble();
         System.out.println("Nilai Ulangan Harian IPA : ");
-            nUHIpa = sc.nextInt();
+            nUHIpa = sc.nextDouble();
         System.out.println("Nilai Ulangan Harian Bahasa Inggris: ");
-            nUHBing = sc.nextInt();
+            nUHBing = sc.nextDouble();
         System.out.println("Nilai Ulangan Harian Bahasa Indonesia: ");
-            nUHBin = sc.nextInt();
+            nUHBin = sc.nextDouble();
         System.out.println("Nilai Ujian Matematika : ");
-            nUMat = sc.nextInt();
+            nUMat = sc.nextDouble();
         System.out.println("Nilai Ujian IPA");
-            nUIpa = sc.nextInt();
+            nUIpa = sc.nextDouble();
         System.out.println("Nilai Ujian Bahasa Inggris");
-            nUBing = sc.nextInt();
+            nUBing = sc.nextDouble();
         System.out.println("Nilai Ujian Bahasa Indonesia");
-            nUBin = sc.nextInt();
+            nUBin = sc.nextDouble();
 
 
         nAIpa = (nIpa + nUHIpa + nUIpa)/jmlPenilaian*bIpa; //cari rata-rata nilai mapel dulu (dibagi 3), lalu kali beban mapel = nilai akhir mapel
