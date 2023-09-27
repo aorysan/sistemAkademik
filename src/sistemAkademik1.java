d649c4cdbc55094cfabecf10133b1945671b7c49
aorysan <aryoadiputro@gmail.com>
2023-09-27 08:29:24 +0700

    hapus bing

--- src/sistemAkademik1.java
+++ src/sistemAkademik1.java
@@ -3,16 +3,15 @@ public class sistemAkademik1 {
     
 public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         int nMat,nIpa, nBing, nBin;
+         int nMat, nIpa, nBin;
          double rataRata;
          
         System.out.println("Masukkan Nilai Tugas :");
         
         nMat = sc.nextInt();
         nIpa = sc.nextInt();
-        nBing = sc.nextInt();
         nBin = sc.nextInt();
-        rataRata = (nMat + nIpa + nBing + nBin) / 4;
+        rataRata = (nMat + nIpa + nBin) / 3;
         
         System.out.println("Rata-rata : " + rataRata);
         sc.close();
