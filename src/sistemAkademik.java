d2b064293b9fe49ee6ef77e051388f9ba307afe6
aorysan <aryoadiputro@gmail.com>
2023-09-27 09:15:05 +0700

    kkm

--- src/sistemAkademik.java
+++ src/sistemAkademik.java
@@ -7,7 +7,7 @@ public class sistemAkademik {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             double nMat, nIpa, nBin, nUHMat, nUHIpa, nUHBin, nUMat, nUIpa, nUBin, bMat = 3, bIpa = 3, bBin = 3;
-            double jmlPenilaian = 3;
+            double jmlPenilaian = 10;
             double rataRata, indeksPrestasi, nAIpa, nAMat, nABin; //int ganti ke double karena input nilai bisa berupa pecahan
             char nilaiHuruf;
                 
@@ -51,38 +51,24 @@ public class sistemAkademik {
         nAMat = (nMat + nUHMat + nUMat)/jmlPenilaian*bMat;
 
             System.out.println("Hasil Nilai Akhir IPA : " + nAIpa);
-                if (nAIpa>80) {
-                    System.out.println("IPA : A");
-                } else if (nAIpa>=65) {
-                    System.out.println("IPA : B");
-                } else if (nAIpa>=50) {
-                    System.out.println("IPA : C");
-                } else {
-                    System.out.println("IPA : D");
-                }
-           
+            if (nAIpa<75) {
+                System.out.println("Remidi");
+            } else {
+                System.out.println("Tidak Remidi");
+            }
             System.out.println("Hasil Nilai Akhir Bahasa Indonesia : " + nABin);
-                if (nABin>80) {
-                    System.out.println("BAHASA INDONESIA : A");
-                } else if (nABin>=65) {
-                    System.out.println("BAHASA INDONESIA : B");
-                } else if (nABin>=50) {
-                    System.out.println("BAHASA INDONESIA : C");
-                } else {
-                    System.out.println("BAHASA INDONESIA : D");
-                }
-
+            if (nABin<75) {
+                System.out.println("Remidi");
+            } else {
+                System.out.println("Tidak Remidi");
+            }
             System.out.println("Hasil Nilai Akhir Matematika : " + nAMat);
-                if (nAMat>80) {
-                    System.out.println("MATEMATIKA : A");
-                } else if (nAMat>=65) {
-                    System.out.println("MATEMATIKA : B");
-                } else if (nAMat>=50) {
-                    System.out.println("MATEMATIKA : C");
-                } else {
-                    System.out.println("MATEMATIKA : D");
-                }
-
+            if (nAMat<75) {
+                System.out.println("Remidi");
+            } else {
+                System.out.println("Tidak Remidi");
+            }
+        
         indeksPrestasi = (nAMat+nAIpa+nABin)/(bMat+bIpa+bBin);
             System.out.println("Indeks Prestasi siswa : " + indeksPrestasi);
         
