afe75f525b1c88b389de88d7cdb8da14e46feb35
aorysan <aryoadiputro@gmail.com>
2023-10-04 08:34:46 +0700

    menambah fitur penghitung presensi dan keterangan lulus

--- src/sistemAkademik.java
+++ src/sistemAkademik.java
@@ -10,39 +10,66 @@ public class sistemAkademik {
             double nMat, nIpa, nBin, nUHMat, nUHIpa, nUHBin, nUMat, nUIpa, nUBin, bMat = 3, bIpa = 3, bBin = 3;
             double jmlPenilaian = 3; //jml penilaian tiap mapel saja (skrng cuma ada tugas, UH, sm ujian)
             double rataRata, indeksPrestasi, nAIpa, nAMat, nABin; //int ganti ke double karena input nilai bisa berupa pecahan
-            char nilaiHuruf;
                 
+            System.out.println("\n\n#########################################");
+            System.out.println("    Selamat Datang di Sistem Akademik    ");
+            System.out.println("#########################################\n");
+
             System.out.println("Masukkan username : ");
             user = sc.nextLine();
-            System.out.println("Masukkan password : ");
+            System.out.println("Masukkan password : \n");
             pass = sc.nextLine();
     
             if (user.equals(userA) && pass.equals(passA)){
-                System.out.println("Login berhasil");
+                System.out.println("Login berhasil\n");
             } else {
-                System.out.println("username atau password salah");
+                System.out.println("username atau password salah\n");
             }
             
-        System.out.println("Nilai Tugas Matematika : ");
+        System.out.println("Masukkan jumlah kehadiran siswa dalam 1 semester : ");
+            int presensi = sc.nextInt();
+        System.out.println("\nNilai Tugas Matematika : ");
             nMat = sc.nextDouble();
-        System.out.println("Nilai Tugas IPA : ");
-            nIpa = sc.nextDouble();
-        System.out.println("Nilai Tugas Bahasa Indonesia : ");
-            nBin = sc.nextDouble();
-
         System.out.println("Nilai Ulangan Harian Matematika : ");
             nUHMat = sc.nextDouble();
-        System.out.println("Nilai Ulangan Harian IPA : ");
-            nUHIpa = sc.nextDouble();
-        System.out.println("Nilai Ulangan Harian Bahasa Indonesia: ");
-            nUHBin = sc.nextDouble();
-
         System.out.println("Nilai Ujian Matematika : ");
             nUMat = sc.nextDouble();
+            nAMat = (nMat + nUHMat + nUMat)/jmlPenilaian*bMat;
+        System.out.println("Hasil Nilai Akhir Matematika : " + nAMat);
+            if (nAMat<75) {
+                System.out.println("Remidi");
+            } else {
+                System.out.println("Tidak Remidi");
+            }
+
+        System.out.println("\nNilai Tugas IPA : ");
+            nIpa = sc.nextDouble();
+        System.out.println("Nilai Ulangan Harian IPA : ");
+            nUHIpa = sc.nextDouble();  
         System.out.println("Nilai Ujian IPA");
             nUIpa = sc.nextDouble();
+            nAIpa = (nIpa + nUHIpa + nUIpa)/jmlPenilaian*bIpa;
+        System.out.println("Hasil Nilai Akhir IPA : " + nAIpa);
+            if (nAIpa<75) {
+                System.out.println("Remidi");
+            } else {
+                System.out.println("Tidak Remidi");
+            }
+
+        System.out.println("\nNilai Tugas Bahasa Indonesia : ");
+            nBin = sc.nextDouble();
+        System.out.println("Nilai Ulangan Harian Bahasa Indonesia: ");
+            nUHBin = sc.nextDouble();
         System.out.println("Nilai Ujian Bahasa Indonesia");
             nUBin = sc.nextDouble();
+            nABin = (nBin + nUHBin + nUBin)/jmlPenilaian*bBin;
+        System.out.println("Hasil Nilai Akhir Bahasa Indonesia : " + nABin);
+            if (nABin<75) {
+                System.out.println("Remidi");
+            } else {
+                System.out.println("Tidak Remidi");
+            }
+            
 
             /* 
         nAIpa = (nIpa + nUHIpa + nUIpa)*bIpa;
@@ -60,39 +87,26 @@ public class sistemAkademik {
         System.out.println("Nilai rata-rata siswa : " + rataRata);
         */ //ini yang lama
 
-        nAIpa = (nIpa + nUHIpa + nUIpa)/jmlPenilaian*bIpa; //cari rata-rata nilai mapel dulu (dibagi 3), lalu kali beban mapel = nilai akhir mapel
-        nABin = (nBin + nUHBin + nUBin)/jmlPenilaian*bBin;
-        nAMat = (nMat + nUHMat + nUMat)/jmlPenilaian*bMat;
+         //cari rata-rata nilai mapel dulu (dibagi 3), lalu kali beban mapel = nilai akhir mapel
+        
+        double jmlPresensi = 100/presensi;
 
-            System.out.println("Hasil Nilai Akhir IPA : " + nAIpa);
-            if (nAIpa<75) {
-                System.out.println("Remidi");
-            } else {
-                System.out.println("Tidak Remidi");
-            }
-            System.out.println("Hasil Nilai Akhir Bahasa Indonesia : " + nABin);
-            if (nABin<75) {
-                System.out.println("Remidi");
-            } else {
-                System.out.println("Tidak Remidi");
-            }
-            System.out.println("Hasil Nilai Akhir Matematika : " + nAMat);
-            if (nAMat<75) {
-                System.out.println("Remidi");
-            } else {
-                System.out.println("Tidak Remidi");
-            }
-        
-        indeksPrestasi = (nAMat+nAIpa+nABin)/(bMat+bIpa+bBin);
-            System.out.println("Indeks Prestasi siswa : " + indeksPrestasi);
-        
-        double ipDalamPersen = indeksPrestasi * 25;
-            System.out.println("IP dalam bentuk persen : " + ipDalamPersen); 
+        indeksPrestasi = (nAMat+nAIpa+nABin)/(225);
+            System.out.println("\nIndeks Prestasi siswa : " + indeksPrestasi);
 
         rataRata = (nAMat+nAIpa+nABin)/(3);
             System.out.println("Nilai rata-rata siswa : " + rataRata);
-        
+
+        if(indeksPrestasi>=3.0) {
+            if(jmlPresensi>=0.7) {
+                System.out.println("\nSiswa memnuhi syarat untuk naik kelas");
+            } else {
+                System.out.println("\nSiswa tidak memenuhi syarat untuk naik kelas");
+            }
+        } else {
+            System.out.println("\nSiswa tidak memenuhi syarat untuk naik kelas");
+        }
+
         sc.close();
-        
     }
 }
